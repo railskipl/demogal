6efbcb8c606aa7b2c7adf5431bf9bc99c20f58f9
railskipl <rails@kunalinfotech.net>
2011-03-31 05:39:03 -0700

    Edited db/migrate/20110222064540_create_pages.rb via GitHub

--- db/migrate/20110222064540_create_pages.rb
+++ db/migrate/20110222064540_create_pages.rb
@@ -1,15 +1,3 @@
 class CreatePages < ActiveRecord::Migration
-  def self.up
-    create_table :pages do |t|
-      t.string :title
-      t.text :body
-      t.string :keyword
-
-      t.timestamps
-    end
-  end
-
-  def self.down
-    drop_table :pages
-  end
+  
 end
